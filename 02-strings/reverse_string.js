a4768cff5ccb9e7b8b4e6cd85bf4f5cda4a842a7
Zubair Khan <zubairmawya@gmail.com>
2021-06-21 11:35:00 +0200

    reverse_string.js

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,8 @@
 // read carefully the instructions
+let myString = "Zubair khan from Pakistan";
+let stringArray = myString.split("");
+console.log(stringArray[1]);
+
+stringArray.reverse();
+console.log(stringArray);
+console.log(stringArray.join(''));
