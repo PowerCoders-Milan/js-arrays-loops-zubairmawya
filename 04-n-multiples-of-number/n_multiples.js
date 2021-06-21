5d64abfba615c5193db8aaab6fefc5c17b197287
Zubair Khan <zubairmawya@gmail.com>
2021-06-21 11:41:58 +0200

    n_multiples.js

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,15 @@
 // function declaration
 const nMultiplesOf = (number, limit) => {
 
-}
+    const multiples = [];
+    let i = 1;
+    while( i <= limit) {
+        
+        multiples.push(number * i);
+        i++
+        
+    }
+    return multiples
 
-console.log(nMultiplesOf(2, 10))
+}
+console.log(nMultiplesOf(2, 10));
